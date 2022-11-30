158661dc99c2cab53e9206723bab089204370f61
Ömer Duran <108549510+farukduran@users.noreply.github.com>
2022-11-30 17:48:03 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+let saveEl = document.getElementById("save-el");
+let gecmisBilgi = localStorage.getItem("gecmisBilgi");
+const toplamEl = document.getElementById("totalPassenger");
+if (gecmisBilgi !== null) {
+  saveEl.textContent = localStorage.getItem("gecmisBilgi");
+} else {
+  saveEl.textContent = "";
+}
+
+let countEl = document.getElementById("count-el");
+let count = 0;
+let toplamYolcu = 0;
+
+function increment() {
+  count += 1;
+  countEl.textContent = count;
+}
+
+function save() {
+  let countStr = count + " - ";
+  saveEl.textContent += countStr;
+  toplamYolcu += count;
+  localStorage.setItem("gecmisBilgi", saveEl.textContent);
+  toplamEl.textContent = toplamYolcu;
+  countEl.textContent = 0;
+  count = 0;
+}
